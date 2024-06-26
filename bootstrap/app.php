5c6756f3ea8f56363ef90d31a946587227c6b429
Tomen1980 <agungjumantoroandrian@gmail.com>
2024-06-26 16:33:17 +0700

    +Make Middleware Timeout Session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\RoleAuthenticated;
+use App\Http\Middleware\AutoLogoutAuth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            "RedirectAuth" => RedirectIfAuthenticated::class,
+            "RoleAuthenticated" => RoleAuthenticated::class,
+            "AutoLogoutAuth" => AutoLogoutAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', [AuthController::class, 'loginForm'])->name('login');
+Route::get('/', [AuthController::class, 'loginForm'])
+    ->name('login')
+    ->middleware('RedirectAuth');
 Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
 Route::post('/loginAction', [AuthController::class, 'loginAuth']);
 
-Route::group(['middleware' => 'auth'], function () {
+Route::middleware(['auth', 'AutoLogoutAuth'])->group(function () {
     Route::get('/dashboard', [AuthController::class, 'dashboard']);
-});
-
-Route::get("/formPeserta", function () {
-    return view('formPeserta');
-});
-
-Route::get("/listPeserta", function () {
-    return view('listPeserta');
+    Route::delete('/logout', [AuthController::class, 'logout']);
+    Route::get('/formPeserta', [UserController::class, 'create'])->middleware('RoleAuthenticated');
+    Route::get('/searchPeserta', [UserController::class, 'searchPeserta'])
+        ->name('searchPeserta')
+        ->middleware('RoleAuthenticated');
+    Route::post('/addPeserta', [UserController::class, 'store'])
+        ->name('addPeserta')
+        ->middleware('RoleAuthenticated');
+    Route::get('/listPeserta', [UserController::class, 'index'])
+        ->name('listPeserta')
+        ->middleware('RoleAuthenticated');
+    Route::delete('/deletePeserta/{id}', [UserController::class, 'destroy'])
+        ->name('deletePeserta')
+        ->middleware('RoleAuthenticated');
+    Route::get('/detailPeserta/{id}', [UserController::class, 'show'])
+        ->name('detailPeserta')
+        ->middleware('RoleAuthenticated');
+    Route::put('/updatePeserta/{id}', [UserController::class, 'update'])
+        ->name('updatePeserta')
+        ->middleware('RoleAuthenticated');
 });
